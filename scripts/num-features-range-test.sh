982107a19d473c1b7eb5c4c0462d22f19d12ae67
Joe <jjpayne@andrew.cmu.edu>
2021-01-27 17:10:10 -0500

    Scripts to test the performance of ORB over a variety of different # of features per frame

---
+++ scripts/num-features-range-test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+### This script is meant to run ORB SLAM 3 on the "longer run" dataset multiple 
+### times and collect a variety of output from it into the designated folders.
+### The version of the RML ORB SLAM fork that should be used with this is xxxxxx
+input="../magnitudesRetrialVsExtraFeatures.txt"
+for j in {1000..10000..1000}
+do
+	for i in {1..100}
+	do
+		while read line
+		do
+			# This is simply for feedback to know how far into the script we are
+			echo "features $j magnitude $i test $line"
+			# Calling ORB-SLAM-3 On the desired dataset
+			./../Examples/Stereo/stereo_kitti ../Vocabulary/ORBvoc.txt ../Examples/Stereo/AirSim-$j.yaml /media/joe/5056599056597824/AirSimResults/PitchMagnitudeLongerRun/$line
+			# Moving the output files to the appropriate location (Make sure to back them up to box)
+			#Trajectories
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/trajectories/
+			mv CameraTrajectory.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/trajectories/trajectory$i.txt"
+			#number of tracked points
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nTrackedPoints/
+			mv nTrackedPoints.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nTrackedPoints/nTrackedPoints$i.txt"
+			#Tracking States
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/trackingStates/
+			mv trackingStates.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/trackingStates/trackingStates$i.txt"
+			#inlier ratios
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/inlierRatios/
+			mv inlierRatios.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/inlierRatios/inlierRatios$i.txt"
+			#average loss
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/averageLoss/
+			mv averageLoss.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/averageLoss/averageLoss$i.txt"
+
+			#Covisibility Graphs
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/covisibility/
+			mv covisibility.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/covisibility/covisibility$i.txt"
+
+			#Number of Map Points
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nMapPoints/
+			mv nMapPoints.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nMapPoints/nMapPoints$i.txt"
+			#% of Keypoints considered close
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nStereoPoints/
+			mv nStereoPoints.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nStereoPoints/nStereoPoints$i.txt"
+
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nCloseStereoPoints/
+			mv nCloseStereoPoints.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nCloseStereoPoints/nCloseStereoPoints$i.txt"
+
+			# Number of re-extractions to successfully complete ORB estimation
+			mkdir --parents /media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nReExtractions/
+			mv reExtractions.txt "/media/joe/5056599056597824/ORBSlam3Recordings/num-features-extracted/$j/$line/nReExtractions/nReExtractions$i.txt"
+
+
+		done < ../magnitudesRetrialVsExtraFeatures.txt
+	done
+done
